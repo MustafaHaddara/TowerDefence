d4efddf85bd7c6c626d6bc9d14f1be5bc28fea18
Bilal <redwings_50@hotmail.com>
2017-03-29 18:37:56 -0400

    nothing

--- tdgame/TDTowerController.cpp
+++ tdgame/TDTowerController.cpp
@@ -5,7 +5,7 @@
 //  Created by Mustafa Haddara on 2017-01-07.
 //
 //
-#include <stdio.h>
+
 #include "TDTowerController.h"
 #include "MMGameWorld.h"
 
@@ -15,179 +15,180 @@
 #include "TSMethods.h"
 #include "TSWorld.h"
 #include "TSZones.h"
-#include "MMMultiplayer.h"
 
 namespace MMGame {
-    using namespace Tombstone;
+	using namespace Tombstone;
 
-    using Tombstone::ControllerType;
-    
-    TowerController::TowerController() : RigidBodyController(kControllerTower) {
-		SetRigidBodyType(kRigidBodyTower);
-		shapeInitFlag = false;
-    }
+	using Tombstone::ControllerType;
 
-    TowerController::~TowerController() {
-    }
-
-    void TowerController::PreprocessController(void) {
-        RigidBodyController::PreprocessController();
-        
-	    myCount = 0;
-        const Node *target = GetTargetNode();
-        originalTransform = target->GetWorldTransform();//GetNodeTransform();
-        Point3D originalDirection = Point3D(originalTransform[0].x,originalTransform[0].y, originalTransform[0].z);
-        originalView = (originalDirection).Normalize();
-        
-        //Find the marker on the Turret. It is the point from which to fire.
-        Node *root = GetTargetNode();
-        Node *thisnode = root;
-        String<30> nodeName = "NoName";
-        bool found = false;
-        do
-        {
-            nodeName = "NoName";
-            if (thisnode->GetNodeName() != NULL)
-                nodeName = thisnode->GetNodeName();
-            if ((Text::CompareText(nodeName, "TurretBarrel")) ) {
-                turretBarrel = thisnode;
-                found = true;
-            }
-            thisnode = root->GetNextTreeNode(thisnode);
-        } while (thisnode && !found); 
-    }
-	void TowerController::towerTakeDamage(int32 damage) {
-		// fighterController => instance of player
-		// 
-
-		health = health - damage;
-		// take damage only called by the server. 
-		TheMessageMgr->SendMessageAll(UpdateTowerHealthMessage(health, GetControllerIndex()));
+	TowerController::TowerController() : Controller(kControllerTower) {
 	}
-    void TowerController::MoveController(void) {
-        /* Only track the server player */
-        if (Tombstone::TheMessageMgr->GetServerFlag()) {
-            myCount += TheTimeMgr->GetDeltaTime();
 
-            Vector3D view = Vector3D(0,0,0);
-            MinionController *mController = GetTargetPoint(RANGE, &view);
-            if (mController) {
-                //interpolate between the current viewing direction and the target viewing direction.
-                //If you use say 40.0 instead of 20.0 then it will move twice as slowly.
-                Vector3D updatedView = Vector3D(originalView.x+(view.x-originalView.x)/40.0f,
-                                                originalView.y+(view.y-originalView.y)/40.0f,
-                                                originalView.z+(view.z-originalView.z)/40.0f);
-                
-                originalView = updatedView;
-                
-                TheMessageMgr->SendMessageAll(TowerRotateMessage(kTowerRotateMessage, updatedView, GetControllerIndex()));
-                
-                // Fire on every SHOOT_DURATION milliseconds
-                if (myCount >= SHOOT_DURATION) {
-                    myCount = 0;
-                    // Fire
-                    mController->DealDamage(DAMAGE_DEALT);
-                }
-            }
-        }
-    }
-    
-    MinionController* TowerController::GetTargetPoint(int32 max_dist, Vector3D *out) {
-        GameWorld *gw = static_cast<GameWorld *>(TheWorldMgr->GetWorld());
-        Node** minions = gw->GetMinions();
-        
-        Point3D startPos = turretBarrel->GetWorldPosition();
-        
-        for (int i=0; i<gw->GetNumMinions(); i++) {
-            if (minions[i] == nullptr) continue;
-            Point3D targetPoint = minions[i]->GetWorldPosition();
-            Box3D *box = new Box3D();
-            MinionController *mc = static_cast<MinionController *>(minions[i]->GetController());
-            if (mc && !minions[i]->CalculateBoundingBox(box)) {
-                // whelp this is a stupid error
-                // should never happen
-                return nullptr;
-            }
+	TowerController::~TowerController() {
+	}
 
-            targetPoint = targetPoint + (2*box->GetCenter()); // I have no idea why multiplying by 2 makes the tracking look better
+	void TowerController::PreprocessController(void) {
+		Controller::PreprocessController();
 
-            Vector3D view = (targetPoint - startPos);
-            float x = view.x, y = view.y, z = view.z;
-            float distance = Sqrt(x * x + y * y + z * z);
-            if (distance < max_dist) {
-                view = view.Normalize();
-                out->x = view.x;
-                out->y = view.y;
-                out->z = view.z;
-                return mc;
-            }
-        }
-        return nullptr;
-    }
+		myCount = 0;
+		const Node *target = GetTargetNode();
+		originalTransform = target->GetWorldTransform();//GetNodeTransform();
+		Point3D originalDirection = Point3D(originalTransform[0].x, originalTransform[0].y, originalTransform[0].z);
+		originalView = (originalDirection).Normalize();
 
-    ControllerMessage *TowerController::CreateMessage(ControllerMessageType type) const {
-        switch (type) {
-            case kTowerRotateMessage:
-                return new TowerRotateMessage(type, GetControllerIndex());
-        }
-        
-        return (Controller::CreateMessage(type));
-    }
+		//Find the marker on the Turret. It is the point from which to fire.
+		Node *root = GetTargetNode();
+		Node *thisnode = root;
+		String<30> nodeName = "NoName";
+		bool found = false;
+		do
+		{
+			nodeName = "NoName";
+			if (thisnode->GetNodeName() != NULL)
+				nodeName = thisnode->GetNodeName();
+			if ((Text::CompareText(nodeName, "TurretBarrel"))) {
+				turretBarrel = thisnode;
+				found = true;
+			}
+			thisnode = root->GetNextTreeNode(thisnode);
+		} while (thisnode && !found);
+	}
 
-    
-    void TowerController::ReceiveMessage(const ControllerMessage *message) {
-        if (message->GetControllerMessageType() == kTowerRotateMessage) {
-            Node *target = GetTargetNode();
-            
-            const TowerRotateMessage *m = static_cast<const TowerRotateMessage *>(message);
-            Vector3D updatedView = m->getTarget();
-            
-            float x = updatedView.x;
-            float y = updatedView.y;
-            float f = InverseSqrt(x * x + y * y);
-            Vector3D right(y * f, -x * f, 0.0F);
-            Vector3D down = Cross(updatedView, right);
-            
-            target->SetNodeMatrix3D(updatedView, -right, -down);
-            // Invalidate the target node so that it gets updated properly
-            target->InvalidateNode();
+	void TowerController::MoveController(void) {
+		/* Only track the server player */
+		if (Tombstone::TheMessageMgr->GetServerFlag()) {
+			myCount += TheTimeMgr->GetDeltaTime();
 
-        } else {
-            Controller::ReceiveMessage(message);
-        }
-    }
-    
-    TowerRotateMessage::TowerRotateMessage(ControllerMessageType type, int32 index): Tombstone::ControllerMessage(type, index) {
-        
-    }
-    
-    TowerRotateMessage::TowerRotateMessage(ControllerMessageType type, const Vector3D& trgt, int32 index, unsigned_int32 flags): Tombstone::ControllerMessage(type, index, flags) {
-        target = trgt;
-    }
+			Vector3D view = Vector3D(0, 0, 0);
+			MinionController *mController = GetTargetPoint(RANGE, &view);
+			if (mController) {
+				//interpolate between the current viewing direction and the target viewing direction.
+				//If you use say 40.0 instead of 20.0 then it will move twice as slowly.
+				Vector3D updatedView = Vector3D(originalView.x + (view.x - originalView.x) / 40.0f,
+					originalView.y + (view.y - originalView.y) / 40.0f,
+					originalView.z + (view.z - originalView.z) / 40.0f);
 
-    TowerRotateMessage::~TowerRotateMessage() {
-        
-    }
+				originalView = updatedView;
 
-    void TowerRotateMessage::CompressMessage(Compressor& data) const {
-        ControllerMessage::CompressMessage(data);
-        
-        data << target.x;
-        data << target.y;
-        data << target.z;
-    }
+				TheMessageMgr->SendMessageAll(TowerRotateMessage(kTowerRotateMessage, updatedView, GetControllerIndex()));
 
-    bool TowerRotateMessage::DecompressMessage(Decompressor& data) {
-        if (ControllerMessage::DecompressMessage(data)) {
-            data >> target.x;
-            data >> target.y;
-            data >> target.z;
-            
-            return true;
-        }
-        
-        return false;
-    }
-		
+				// Fire on every SHOOT_DURATION milliseconds
+				if (myCount >= SHOOT_DURATION) {
+					myCount = 0;
+					// Fire
+					mController->DealDamage(DAMAGE_DEALT);
+				}
+			}
+		}
+	}
+
+	MinionController* TowerController::GetTargetPoint(int32 max_dist, Vector3D *out) {
+		// if we're tracking something already
+		if (currentTarget != nullptr) {
+			if (OrientationToMinion(currentTarget, max_dist, out)) {
+				MinionController *mc = static_cast<MinionController *>(currentTarget->GetController());
+				return mc;
+			}
+			else {
+				// minion got out of our range
+				currentTarget = nullptr;
+			}
+		}
+
+		GameWorld *gw = static_cast<GameWorld *>(TheWorldMgr->GetWorld());
+		Node** minions = gw->GetMinions();
+
+		for (int i = 0; i<gw->GetNumMinions(); i++) {
+			if (minions[i] == nullptr) continue;
+			if (OrientationToMinion(minions[i], max_dist, out)) {
+				MinionController *mc = static_cast<MinionController *>(currentTarget->GetController());
+				return mc;
+			}
+		}
+		return nullptr;
+	}
+
+	bool TowerController::OrientationToMinion(Node* minion, int32 max_dist, Vector3D* out) {
+		Point3D startPos = turretBarrel->GetWorldPosition();
+
+		Point3D targetPoint = minion->GetWorldPosition();
+
+		Vector3D view = (targetPoint - startPos);
+		float distance = Magnitude(view);
+		if (distance < max_dist) {
+			view = view.Normalize();
+			out->x = view.x;
+			out->y = view.y;
+			out->z = view.z;
+			currentTarget = minion;
+			return true;
+		}
+		return false;
+	}
+
+	ControllerMessage *TowerController::CreateMessage(ControllerMessageType type) const {
+		switch (type) {
+		case kTowerRotateMessage:
+			return new TowerRotateMessage(type, GetControllerIndex());
+		}
+
+		return (Controller::CreateMessage(type));
+	}
+
+
+	void TowerController::ReceiveMessage(const ControllerMessage *message) {
+		if (message->GetControllerMessageType() == kTowerRotateMessage) {
+			Node *target = GetTargetNode();
+
+			const TowerRotateMessage *m = static_cast<const TowerRotateMessage *>(message);
+			Vector3D updatedView = m->getTarget();
+
+			float x = updatedView.x;
+			float y = updatedView.y;
+			float f = InverseSqrt(x * x + y * y);
+			Vector3D right(y * f, -x * f, 0.0F);
+			Vector3D down = Cross(updatedView, right);
+
+			target->SetNodeMatrix3D(updatedView, -right, -down);
+			// Invalidate the target node so that it gets updated properly
+			target->InvalidateNode();
+
+		}
+		else {
+			Controller::ReceiveMessage(message);
+		}
+	}
+
+	TowerRotateMessage::TowerRotateMessage(ControllerMessageType type, int32 index) : Tombstone::ControllerMessage(type, index) {
+
+	}
+
+	TowerRotateMessage::TowerRotateMessage(ControllerMessageType type, const Vector3D& trgt, int32 index, unsigned_int32 flags) : Tombstone::ControllerMessage(type, index, flags) {
+		target = trgt;
+	}
+
+	TowerRotateMessage::~TowerRotateMessage() {
+
+	}
+
+	void TowerRotateMessage::CompressMessage(Compressor& data) const {
+		ControllerMessage::CompressMessage(data);
+
+		data << target.x;
+		data << target.y;
+		data << target.z;
+	}
+
+	bool TowerRotateMessage::DecompressMessage(Decompressor& data) {
+		if (ControllerMessage::DecompressMessage(data)) {
+			data >> target.x;
+			data >> target.y;
+			data >> target.z;
+
+			return true;
+		}
+
+		return false;
+	}
 
 }
--- tdgame/TDTowerController.h
+++ tdgame/TDTowerController.h
@@ -7,37 +7,37 @@
 
 namespace MMGame
 {
-    using namespace Tombstone;
-    
-    // Controller types
-    enum
-    {
-        kControllerTower        = 'Turr'
-    };
-    
-    enum
-    {
-        kModelTower             = 'Turt'
-    };
-    
-    class TowerController final : public RigidBodyController {
-    private:
+	using namespace Tombstone;
+
+	// Controller types
+	enum
+	{
+		kControllerTower = 'Turr'
+	};
+
+	enum
+	{
+		kModelTower = 'Turt'
+	};
+
+	class TowerController final : public RigidBodyController {
+	private:
 		int32			health = 50;
 		bool			shapeInitFlag;
 
-        const int       SHOOT_DURATION = 1000 * 2;  // Fire every 2 seconds
-        const float     RANGE = 30.0f;              // Range within aiming
-        const int       DAMAGE_DEALT = 20;          // Damage per shot
-        
-        int             myCount;                    // for firing
-        Transform4D     originalTransform;          // The target's original transform
-        Vector3D        originalView;               // The direction turret is facing before any rotation
-        Node			*turretBarrel;              // Marker on Turret Model from which to shoot
-        
-    public:
-        TowerController();
-        ~TowerController();
-        void PreprocessController(void) override;
+		const int       SHOOT_DURATION = 1000 * 2;  // Fire every 2 seconds
+		const float     RANGE = 30.0f;              // Range within aiming
+		const int       DAMAGE_DEALT = 20;          // Damage per shot
+
+		int             myCount;                    // for firing
+		Transform4D     originalTransform;          // The target's original transform
+		Vector3D        originalView;               // The direction turret is facing before any rotation
+		Node			*turretBarrel;              // Marker on Turret Model from which to shoot
+
+	public:
+		TowerController();
+		~TowerController();
+		void PreprocessController(void) override;
 		void towerTakeDamage(int32 damage);
 
 		int32 getTowerHealth(void)
@@ -50,36 +50,36 @@ namespace MMGame
 			health = newHealth;
 		}
 		void MoveController(void) override;
-        MinionController* GetTargetPoint(int32 range, Vector3D *out);
-        ControllerMessage *CreateMessage(ControllerMessageType type) const override;
-        void ReceiveMessage(const ControllerMessage *message) override;
-        
-        enum {
-            kTowerRotateMessage,
-        };
-        
-    };
-    
-    class TowerRotateMessage : public ControllerMessage {
-        friend class TowerController;
-        
-    private:
+		MinionController* GetTargetPoint(int32 range, Vector3D *out);
+		ControllerMessage *CreateMessage(ControllerMessageType type) const override;
+		void ReceiveMessage(const ControllerMessage *message) override;
+
+		enum {
+			kTowerRotateMessage,
+		};
+
+	};
+
+	class TowerRotateMessage : public ControllerMessage {
+		friend class TowerController;
+
+	private:
 		int32 health = 300;
-        Vector3D target;
-        
-    public:
-        
-        TowerRotateMessage(ControllerMessageType type, int32 index);
-        TowerRotateMessage(ControllerMessageType type, const Vector3D& trgt, int32 index, unsigned_int32 flags = 0);
-        ~TowerRotateMessage();
-		
-        Vector3D getTarget() const {
-            return target;
-        }
-        
-        void CompressMessage(Compressor& data) const override;
-        bool DecompressMessage(Decompressor& data) override;
-    };
+		Vector3D target;
+
+	public:
+
+		TowerRotateMessage(ControllerMessageType type, int32 index);
+		TowerRotateMessage(ControllerMessageType type, const Vector3D& trgt, int32 index, unsigned_int32 flags = 0);
+		~TowerRotateMessage();
+
+		Vector3D getTarget() const {
+			return target;
+		}
+
+		void CompressMessage(Compressor& data) const override;
+		bool DecompressMessage(Decompressor& data) override;
+	};
 
 }
 
